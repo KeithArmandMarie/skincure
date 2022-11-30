6ac040cfd8a85cda88c68f026586ef8484a24c3e
Keith Caballero <caballerokeith2@gmail.com>
2022-11-30 15:57:41 +0800

    fixed db

--- skincure/admin.py
+++ skincure/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from skincure.models import SkinDisease, Treatment
+from skincure.models import SkinDisease, Treatment, Symptomp, Case
 # Register your models here.
 @admin.register(SkinDisease)
-class skinDiseaseAdmin(admin.ModelAdmin):
+class SkinDiseaseAdmin(admin.ModelAdmin):
     list_display = ['name']
 @admin.register(Treatment)
 class TreatmentAdmin(admin.ModelAdmin):
-    list_display = ['id','description']
+    list_display = ['name']
+@admin.register(Symptomp)
+class SymptompAdmin(admin.ModelAdmin):
+    list_display = ['name']
+@admin.register(Case)
+class CaseAdmin(admin.ModelAdmin):
+    list_display = ['name']
--- skincure/views.py
+++ skincure/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
-from .models import Treatment, SkinDisease
-from .serializers import SkinDiseaseSerializer, TreatmentSerializers
+from .models import Treatment, SkinDisease, Symptomp, Case
+from .serializers import SkinDiseaseSerializer, TreatmentSerializers, SymptompSerializers, CaseSerializers
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
 # Create your views here.
 
@@ -22,3 +22,21 @@ class SkinDiseaseRetrieveViewSet(RetrieveUpdateDestroyAPIView):
     lookup_field = 'id'
     queryset = SkinDisease.objects.all()
     serializer_class = SkinDiseaseSerializer
+    
+class SymptompViewSet(ListCreateAPIView):
+    queryset = Symptomp.objects.all()
+    serializer_class = SymptompSerializers
+    
+class SymptompRetrieveViewSet(RetrieveUpdateDestroyAPIView):
+    lookup_field = 'id'
+    queryset = Symptomp.objects.all()
+    serializer_class = SymptompSerializers
+    
+class CaseViewSet(ListCreateAPIView):
+    queryset = Case.objects.all()
+    serializer_class = CaseSerializers
+    
+class CaseRetrieveViewSet(RetrieveUpdateDestroyAPIView):
+    lookup_field = 'id'
+    queryset = Case.objects.all()
+    serializer_class = CaseSerializers
